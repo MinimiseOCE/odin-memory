afdc24b1f11590e2b04d82e78bda707e3299a35d
Minimise <jackson.whiteau@gmail.com>
2022-08-11 23:59:43 +0800

    Updates

---
+++ precache-manifest.60778b4a00dcfc015fa67aacb5178452.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "020c5420a485b0c31a2c",
+    "url": "/odin-memory/static/css/main.cc8ce5f9.chunk.css"
+  },
+  {
+    "revision": "020c5420a485b0c31a2c",
+    "url": "/odin-memory/static/js/main.020c5420.chunk.js"
+  },
+  {
+    "revision": "5e1be881a18840e3303a",
+    "url": "/odin-memory/static/js/1.5e1be881.chunk.js"
+  },
+  {
+    "revision": "3a097e4d79fbcb41379f",
+    "url": "/odin-memory/static/js/runtime~main.3a097e4d.js"
+  },
+  {
+    "revision": "af0d62d87b8c28a0c9eefe31de5bc105",
+    "url": "/odin-memory/static/media/hammerhead.af0d62d8.jpg"
+  },
+  {
+    "revision": "f548047bb404393082c79d99a37fb47d",
+    "url": "/odin-memory/static/media/anchovies.f548047b.jpg"
+  },
+  {
+    "revision": "cc2e7b4b0dd76cd369e851f04f45fd13",
+    "url": "/odin-memory/static/media/barramundi.cc2e7b4b.jpg"
+  },
+  {
+    "revision": "cef5a83d908e1ac01f81237cda75e2ee",
+    "url": "/odin-memory/static/media/blobfish.cef5a83d.jpg"
+  },
+  {
+    "revision": "f1278bc2d12dccbeeff558a260537ef8",
+    "url": "/odin-memory/static/media/blueShark.f1278bc2.jpg"
+  },
+  {
+    "revision": "cfc904e40ff68856f06dd26cac96071a",
+    "url": "/odin-memory/static/media/catfish.cfc904e4.jpg"
+  },
+  {
+    "revision": "9fd2989c08a535930c06d60d174ea597",
+    "url": "/odin-memory/static/media/pollock.9fd2989c.jpg"
+  },
+  {
+    "revision": "488d8bb2c80ab9f3e15275b93b97803a",
+    "url": "/odin-memory/static/media/rockfish.488d8bb2.jpg"
+  },
+  {
+    "revision": "83f82d1d92761c1b8cd2c165388199b7",
+    "url": "/odin-memory/static/media/whiting.83f82d1d.jpg"
+  },
+  {
+    "revision": "62f01c894897bd47ccb571d37cd59e1d",
+    "url": "/odin-memory/index.html"
+  }
+];
